7b8b12a5404fc17dad55f686864464653572a5cf
Houston Salgado <houstonsistemas@gmail.com>
2026-08-05 17:34:37 -0700

    Course new changes

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookiesSession = require("cookie-session");
+const passport = require("passport");
+const authRoutes = require("./routes/authRoutes");
+const { mongoURI } = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+const dns = require("dns");
+const keys = require("./config/keys");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookiesSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookiesKey] }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
